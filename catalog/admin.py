153086cd608ab01629f74437a7e0e3a594941f4a
papingothegamer <toluwanimidaramola8@gmail.com>
2026-03-24 17:02:29 +0200

    redesign homepage with animated bookshelf

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Genre, Language, Author, Book, BookInstance
 
+admin.site.site_header = "LocalLibrary Admin"
+admin.site.site_title = "LocalLibrary"
+admin.site.index_title = "Library Management"
+
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
@@ -15,14 +19,19 @@ class LanguageAdmin(admin.ModelAdmin):
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    search_fields = ('first_name', 'last_name')
+    ordering = ('last_name',)
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre', 'language')
+    search_fields = ('title', 'author__last_name')
+    list_filter = ('genre', 'language')
 
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('id', 'book', 'status', 'borrower', 'due_back')
+    list_display = ('book', 'status', 'borrower', 'due_back')
     list_filter = ('status',)
+    search_fields = ('book__title',)
--- catalog/views.py
+++ catalog/views.py
@@ -2,15 +2,34 @@ import django
 from django.shortcuts import render
 from .models import Book, BookInstance, Author, Genre
 
+SPINE_COLORS = [
+    '#2E4057', '#E76F51', '#2A9D8F', '#E9C46A', '#9B5DE5',
+    '#F72585', '#4361EE', '#F4A261', '#264653', '#A8DADC',
+    '#C77DFF', '#06D6A0', '#EF233C', '#FB8500', '#3A86FF',
+    '#8338EC', '#FF006E', '#FFBE0B', '#3D405B', '#81B29A',
+]
+
+SPINE_HEIGHTS = [
+    155, 170, 145, 180, 160, 140, 175, 150, 165, 185,
+    148, 172, 158, 142, 168, 178, 153, 163, 147, 182,
+]
+
 
 def index(request):
+    books = Book.objects.all().order_by('id')
+    shelf_books = []
+    for i, book in enumerate(books):
+        book.spine_color = SPINE_COLORS[i % len(SPINE_COLORS)]
+        book.spine_height = SPINE_HEIGHTS[i % len(SPINE_HEIGHTS)]
+        shelf_books.append(book)
+
     context = {
         'num_books': Book.objects.count(),
         'num_instances': BookInstance.objects.count(),
         'num_instances_available': BookInstance.objects.filter(status='a').count(),
         'num_authors': Author.objects.count(),
         'num_genres': Genre.objects.count(),
-        'recent_books': Book.objects.order_by('-id')[:5],
+        'shelf_books': shelf_books,
         'django_version': django.get_version(),
     }
     return render(request, 'catalog/index.html', context)
