ce73027c8a16773a53e6f3ba577ee7e4b722e4a7
Student_EN <toluwanimidaramola8@gmail.com>
2026-03-27 13:26:51 +0100

    Refactor Author Model: Combined Name Fields

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,34 +1,28 @@
-from django.contrib import admin
+﻿from django.contrib import admin
 from .models import Genre, Language, Author, Book, BookInstance
 
 admin.site.site_header = "LocalLibrary Admin"
 admin.site.site_title = "LocalLibrary"
 admin.site.index_title = "Library Management"
 
-
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
     pass
-
 
 @admin.register(Language)
 class LanguageAdmin(admin.ModelAdmin):
     pass
 
-
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-    search_fields = ('first_name', 'last_name')
-    ordering = ('last_name',)
-
+    list_display = ('full_name', 'date_of_birth', 'date_of_death')
+    search_fields = ('full_name',)
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre', 'language')
-    search_fields = ('title', 'author__last_name')
+    search_fields = ('title', 'author__full_name')
     list_filter = ('genre', 'language')
-
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
--- catalog/models.py
+++ catalog/models.py
@@ -1,39 +1,29 @@
-from django.db import models
+﻿from django.db import models
 from django.urls import reverse
 import uuid
 from django.contrib.auth.models import User
 from datetime import date
 
-
 class Genre(models.Model):
     name = models.CharField(max_length=200, help_text='Enter a book genre')
-
     def __str__(self):
         return self.name
-
 
 class Language(models.Model):
     name = models.CharField(max_length=200, help_text='Enter the book language')
-
     def __str__(self):
         return self.name
 
-
 class Author(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    full_name = models.CharField(max_length=200, help_text='Enter author name (e.g., Last, First)')
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('Died', null=True, blank=True)
-
     class Meta:
-        ordering = ['last_name', 'first_name']
-
+        ordering = ['full_name']
     def get_absolute_url(self):
         return reverse('author-detail', args=[str(self.id)])
-
     def __str__(self):
-        return f'{self.last_name}, {self.first_name}'
-
+        return self.full_name
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -42,18 +32,13 @@ class Book(models.Model):
     isbn = models.CharField('ISBN', max_length=13, unique=True, help_text='13 Character ISBN number')
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
     language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True, blank=True)
-
     def display_genre(self):
         return ', '.join(genre.name for genre in self.genre.all()[:3])
-
     display_genre.short_description = 'Genre'
-
     def __str__(self):
         return self.title
-
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
-
 
 class BookInstance(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this book copy')
@@ -61,26 +46,18 @@ class BookInstance(models.Model):
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
     borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-
     LOAN_STATUS = (
         ('m', 'Maintenance'),
         ('o', 'On loan'),
         ('a', 'Available'),
         ('r', 'Reserved'),
     )
-
-    status = models.CharField(
-        max_length=1, choices=LOAN_STATUS, blank=True, default='m',
-        help_text='Book availability'
-    )
-
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
     @property
     def is_overdue(self):
         return bool(self.due_back and date.today() > self.due_back)
-
     class Meta:
         ordering = ['due_back']
         permissions = (("can_mark_returned", "Set book as returned"),)
-
     def __str__(self):
         return f'{self.id} ({self.book.title})'
