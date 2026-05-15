9212a7d9c9137beba52df601d19318bc069463f7
papingothegamer <toluwanimidaramola8@gmail.com>
2026-05-15 12:14:19 +0300

    Bugfix: Restored the signup route to URLs

--- catalog/urls.py
+++ catalog/urls.py
@@ -19,4 +19,7 @@ urlpatterns = [
     path('book/create/', views.BookCreate.as_view(), name='book-create'),
     path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book-update'),
     path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book-delete'),
+    
+    # Restored the signup route!
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
